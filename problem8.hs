e17900eb907e70a0753d0dd0a0fa3b58232ac322
jiayuanmark <jiayuanmark@gmail.com>
2013-12-24 22:34:29 +0800

    check

---
+++ problem8.hs
@@ -0,0 +1,10 @@
+
+removeDup :: Eq a=>[a]->a->[a]
+removeDup [] _ = []
+removeDup (x:xs) y = if x == y
+						then (removeDup xs x)
+						else [x] ++ (removeDup xs x)
+
+myCompress :: Eq a=>[a]->[a]
+myCompress [] = []
+myCompress (x:xs) = [x] ++ (removeDup xs x)
